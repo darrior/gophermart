01a1abb2aad5a16697386a8ae579cc64db758485
Andrey Lomovtsev <andrei150202@gmail.com>
2026-01-14 22:42:45 +0300

    dev: write syncqueue

---
+++ internal/utils/syncqueue/syncqueue.go
@@ -0,0 +1,41 @@
+// Package syncqueue provides threadsafe queue.
+package syncqueue
+
+import "sync"
+
+type SyncQueue[T any] struct {
+	lock  *sync.Mutex
+	queue []T
+}
+
+func NewSyncQueue[T any]() *SyncQueue[T] {
+	return &SyncQueue[T]{}
+}
+
+func (s *SyncQueue[T]) Pop() (T, bool) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if len(s.queue) == 0 {
+		var ret T
+		return ret, false
+	}
+
+	first := s.queue[0]
+	s.queue = s.queue[1:]
+	return first, true
+}
+
+func (s *SyncQueue[T]) Push(element T) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.queue = append(s.queue, element)
+}
+
+func (s *SyncQueue[t]) IsEmpty() bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	return len(s.queue) == 0
+}
