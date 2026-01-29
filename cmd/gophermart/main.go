fdc53dbca3f27a7ebb4f19f962df9050bdf9fb64
Andrey Lomovtsev <andrei150202@gmail.com>
2026-01-29 20:26:08 +0300

    dev: write main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,46 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"database/sql"
+	"os/signal"
+	"syscall"
+
+	"github.com/darrior/gophermart/cmd/gophermart/config"
+	"github.com/darrior/gophermart/internal/gateways/accrual"
+	"github.com/darrior/gophermart/internal/handlers"
+	"github.com/darrior/gophermart/internal/repository"
+	"github.com/darrior/gophermart/internal/repository/migrations"
+	"github.com/darrior/gophermart/internal/service"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	cfg, err := config.ParseConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Cannot parse config")
+	}
+
+	db, err := sql.Open("pgx", cfg.DatabaseConnConf.ConnString())
+	if err != nil {
+		log.Fatal().Err(err).Msg("Cannot open DB")
+	}
+
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	if err := migrations.Up(ctx, db); err != nil {
+		log.Fatal().Err(err).Msg("Cannot migrate DB")
+	}
+
+	r := repository.NewRepository(db)
+
+	a := accrual.NewAccrual(cfg.AccrualSystemAddress)
+	s := service.NewService(ctx, r, a, 5)
+	srv := handlers.NewServer(cfg.RunAddress, s)
+
+	if err := srv.Start(ctx); err != nil {
+		log.Error().Err(err).Msg("Unexpected shutdown of server")
+	}
+}
