794f1b51651733c172565c2f975ede1fa9e3ad3c
Andrey Lomovtsev <andrei150202@gmail.com>
2026-01-31 15:37:42 +0300

    dev: write unit-tests for internal/service/utils.go

--- internal/service/utils.go
+++ internal/service/utils.go
@@ -2,10 +2,12 @@ package service
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 )
 
 const timeLayout = "2006-01-02T15:04:05-07:00"
 
 func getPasswordHash(password string) string {
-	return string(sha256.New().Sum([]byte(password)))
+	sum := sha256.Sum256([]byte(password))
+	return hex.EncodeToString(sum[:])
 }
