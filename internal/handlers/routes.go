b7fff7f7ab8abb853788f2ee168b8433b4eb4fd0
Andrey Lomovtsev <andrei150202@gmail.com>
2026-03-01 17:17:09 +0300

    debug: fix routes

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -11,7 +11,7 @@ func (s *server) setRoutes() {
 			r.Use(s.h.authMiddlware)
 
 			r.Post("/orders", s.h.postAPIUserOrders)
-			r.Post("balance/withdraw", s.h.postAPIUserBalanceWithdraw)
+			r.Post("/balance/withdraw", s.h.postAPIUserBalanceWithdraw)
 			r.Get("/orders", s.h.getAPIUserOrders)
 			r.Get("/balance", s.h.getAPIUserBalance)
 			r.Get("/withdrawals", s.h.getAPIUserWithdrawals)
