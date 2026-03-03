f78f4681d0d29e6cb00fe1ecfe8dd3d74472f451
Andrey Lomovtsev <andrei150202@gmail.com>
2026-03-03 20:21:18 +0300

    fix: remove unused Down function

--- internal/repository/migrations/migrations.go
+++ internal/repository/migrations/migrations.go
@@ -22,16 +22,3 @@ func Up(ctx context.Context, db *sql.DB) error {
 
 	return nil
 }
-
-func Down(ctx context.Context, db *sql.DB) error {
-	goose.SetBaseFS(migrations.EmbedMigrations)
-	if err := goose.SetDialect("postgres"); err != nil {
-		return fmt.Errorf("cannot set dialect: %w", err)
-	}
-
-	if err := goose.DownContext(ctx, db, "."); err != nil {
-		return fmt.Errorf("cannot migrate DB: %w", err)
-	}
-
-	return nil
-}
