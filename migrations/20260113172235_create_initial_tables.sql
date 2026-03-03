0f2adcaeaa75e5922a1a81ac309de5eb13a678a0
Andrey Lomovtsev <andrei150202@gmail.com>
2026-03-03 20:20:28 +0300

    fix: rewrite migrations

--- migrations/20260113172235_create_initial_tables.sql
+++ migrations/20260113172235_create_initial_tables.sql
@@ -2,14 +2,14 @@
 -- +goose StatementBegin
 CREATE TABLE users(
     id uuid NOT NULL PRIMARY KEY,
-    login text,
+    login varchar(32),
     password_hash text,
     current_balance float,
     withdrawan_balance float
 );
 CREATE TYPE order_status AS ENUM ('NEW', 'PROCESSING', 'INVALID', 'PROCESSED');
 CREATE TABLE orders(
-    number text PRIMARY KEY,
+    number varchar(64) PRIMARY KEY,
     accrual float,
     user_uuid uuid,
     status order_status,
@@ -18,10 +18,12 @@ CREATE TABLE orders(
 CREATE TABLE withdrawals(
     id SERIAL PRIMARY KEY,
     user_uuid uuid,
-    order_number text,
+    order_number varchar(64),
     sum float,
     processed_at timestamp
 );
+CREATE UNIQUE INDEX unique_numbers ON orders (number);
+CREATE UNIQUE INDEX unique_logins ON users (login);
 -- +goose StatementEnd
 
 -- +goose Down
@@ -30,4 +32,6 @@ DROP TABLE IF EXISTS withdrawals;
 DROP TABLE IF EXISTS orders;
 DROP TYPE IF EXISTS order_status;
 DROP TABLE IF EXISTS users;
+DROP INDEX IF EXISTS unique_logins;
+DROP INDEX IF EXISTS unique_numbers;
 -- +goose StatementEnd
